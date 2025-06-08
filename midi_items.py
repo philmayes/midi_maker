4d536a70df5fce1f046d2b817a6c04dbad4b1514
Phil Mayes <philmayes@users.noreply.github.com>
2025-06-07 19:03:49 -0700

    Shuffle classes into alpha order

--- midi_items.py
+++ midi_items.py
@@ -1,9 +1,48 @@
 from midi_channels import Channel
 
-
 class Item:
     """Abstract class constituent of a composition."""
     pass
+
+class Bar(Item):
+    """Bar description of a composition."""
+    def __init__(self, chord: str,
+                 repeat: int=1,
+                 ):
+        self.chord = chord
+        self.repeat = repeat
+        self.clip = False
+
+class Loop(Item):
+    """Start a loop."""
+    def __init__(self):
+        pass
+
+class LoopItem:
+    """Item that is pushed onto the loop stack."""
+    def __init__(self, item_no: int, count: int):
+        self.item_no = item_no
+        self.count = count
+
+class Mute(Item):
+    """Mute channel(s)."""
+    def __init__(self, channels: list[Channel]):
+        self.channels = channels
+
+class Play(Item):
+    """Play (unmute) channel(s)."""
+    def __init__(self, channels: list[Channel]):
+        self.channels = channels
+
+class Repeat(Item):
+    start = -1
+    """Repeat a loop one or more times."""
+    def __init__(self, repeat: int=2):
+        # <repeat> is the number of times that control is returned to a Loop()
+        # instance, so Repeat() or Repeat(2) means play the passage twice, and
+        # we should only jump back once.
+        assert repeat >= 2, 'Bad repeat count'
+        self.repeat = repeat - 1
 
 class Tempo(Item):
     """Change the tempo of a composition."""
@@ -24,46 +63,6 @@ class Volume(Item):
     def __init__(self, delta: int, channels: list[Channel]):
         self.delta = delta
         self.channels = channels
-
-class Mute(Item):
-    """Mute channel(s)."""
-    def __init__(self, channels: list[Channel]):
-        self.channels = channels
-
-class Play(Item):
-    """Play (unmute) channel(s)."""
-    def __init__(self, channels: list[Channel]):
-        self.channels = channels
-
-class Bar(Item):
-    """Bar description of a composition."""
-    def __init__(self, chord: str,
-                 repeat: int=1,
-                 ):
-        self.chord = chord
-        self.repeat = repeat
-        self.clip = False
-
-class Loop(Item):
-    """Start a loop."""
-    def __init__(self):
-        pass
-
-class Repeat(Item):
-    start = -1
-    """Repeat a loop one or more times."""
-    def __init__(self, repeat: int=2):
-        # <repeat> is the number of times that control is returned to a Loop()
-        # instance, so Repeat() or Repeat(2) means play the passage twice, and
-        # we should only jump back once.
-        assert repeat >= 2, 'Bad repeat count'
-        self.repeat = repeat - 1
-
-class LoopItem:
-    """Item that is pushed onto the loop stack."""
-    def __init__(self, item_no: int, count: int):
-        self.item_no = item_no
-        self.count = count
 
 class Composition:
     """Collection of items that will generate a MIDI file."""
