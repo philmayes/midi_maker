bc5ca9128fa1e385cb6057b8ad2eefaffdb3ede5
Phil Mayes <philmayes@users.noreply.github.com>
2025-06-17 14:38:33 -0700

    Invert meaning of -p parameter

--- midi.py
+++ midi.py
@@ -493,7 +493,7 @@ if __name__=='__main__':
     parser.add_argument('ini', nargs='?', default='example.ini', help=f'Data to create MIDI file (default: example.ini)')
     parser.add_argument('-l', '--log', default=default_log_level, help='logging level')
     parser.add_argument('-c', '--create', default='', help='create composition or opus')
-    parser.add_argument('-p', '--play', default=True, help='play the generated midi file')
+    parser.add_argument('-p', '--play', action="store_true", default=False, help='play the generated midi file')
     args = parser.parse_args()
     logging.basicConfig(format='%(message)s', level=get_logging_level(args))
 
