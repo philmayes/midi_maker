b27d0e19bf61a3494a1fd41d877972578d83cc0d
Phil Mayes <philmayes@users.noreply.github.com>
2025-06-20 09:45:24 -0700

    Add version

--- midi_maker.py
+++ midi_maker.py
@@ -5,6 +5,11 @@ import os
 from midi import make_midi
 import midi_play
 
+major = 0
+minor = 0
+patch = 0
+version = f'{major}.{minor}.{patch}'
+
 log_levels = (
     'DEBUG',    # Detailed information, typically of interest only when
                 # diagnosing problems.
@@ -29,6 +34,9 @@ def get_logging_level(args:argparse.Namespace) -> str:
     return default_log_level
 
 def run(args:argparse.Namespace):
+    if args.version:
+        print(f'Version {version}')
+        return
     in_file = args.ini
     if not os.path.exists(in_file):
         logging.critical(f'Input file "{in_file}" does not exist')
@@ -44,11 +52,13 @@ def run(args:argparse.Namespace):
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Create MIDI file')
     parser.add_argument('ini', nargs='?', default='example.ini', help=f'Data to create MIDI file (default: example.ini)')
+    parser.add_argument('-v', '--version', action="store_true", help='version')
     parser.add_argument('-l', '--log', default=default_log_level, help='logging level')
     parser.add_argument('-c', '--create', default='', help='create composition or opus')
     parser.add_argument('-p', '--play', action="store_true", default=False, help='play the generated midi file')
     args = parser.parse_args()
     logging.basicConfig(format='%(message)s', level=get_logging_level(args))
+
 
     try:
         run(args)
